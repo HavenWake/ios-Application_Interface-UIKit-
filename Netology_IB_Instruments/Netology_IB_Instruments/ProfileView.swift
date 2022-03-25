37912d01721f6551b9ee13d9145f3c5130a1bedb
Haven <havenwake@icloud.com>
2022-03-25 11:44:58 +0300

    add changes

---
+++ Netology_IB_Instruments/Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Семён Пряничников on 25.03.2022.
+//
+
+import Foundation
+import UIKit
+
+class ProfileView: UIView {
+    @IBOutlet weak var photo: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var birthday: UILabel!
+    @IBOutlet weak var country: UILabel!
+    @IBOutlet weak var signature: UITextView!
+}
---
+++ Netology_IB_Instruments/Netology_IB_Instruments/ProfileViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by Семён Пряничников on 25.03.2022.
+//
+
+import Foundation
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+}
