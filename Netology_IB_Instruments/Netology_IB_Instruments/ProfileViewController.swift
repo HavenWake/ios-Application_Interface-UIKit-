6e8d8c5577fcb0a2f9e250b948db2216f257fe88
Haven <havenwake@icloud.com>
2022-03-31 09:05:57 +0300

    add subview

--- Netology_IB_Instruments/Netology_IB_Instruments/ProfileViewController.swift
+++ Netology_IB_Instruments/Netology_IB_Instruments/ProfileViewController.swift
@@ -9,5 +9,14 @@ import Foundation
 import UIKit
 
 class ProfileViewController: UIViewController {
-
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let screenWidth = UIScreen.main.bounds.width
+        if let profileView = Bundle.main.loadNibNamed("ProfileView", owner: nil, options: nil)?.first as? ProfileView {
+            profileView.frame = CGRect(x: 0, y: 0, width: screenWidth, height: 0)
+            view.addSubview(profileView)
+        }
+    }
+    
 }
