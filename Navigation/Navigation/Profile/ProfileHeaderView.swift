7ad5712c29444d9b4a6e4819bc8ddd0c3548b508
Haven <havenwake@icloud.com>
2022-04-04 14:07:25 +0300

    Squashed commit of the following:
    commit 236ff996c7cef0ce325c095241282c5225168cf9
    Author: Haven <havenwake@icloud.com>
    Date:   Mon Apr 4 14:00:00 2022 +0300
    
        new changes added
    
    commit d670da8809c0854a83c267d88b3becf904f216a0
    Author: Haven <havenwake@icloud.com>
    Date:   Wed Mar 30 17:48:37 2022 +0300
    
        progress in homework 2.1
    
        add Profile Group
        add ProfileHeaderView
        set lightGray background color on ProfileViewController
    
    commit aa83c948eca14bdbf05c44c433d66625384328eb
    Author: Haven <havenwake@icloud.com>
    Date:   Wed Mar 30 14:49:10 2022 +0300
    
        Squashed commit of the following:
    
        commit 5f36c0c5e42d8bae0aa2b61568ea0537611f7420
        Author: Haven <havenwake@icloud.com>
        Date:   Wed Mar 30 14:40:36 2022 +0300
    
            add homework 1.3
    
            1. Создан проект Navigation
            2. Удалён Main.Storyboard
            3. В AppDelegate добавлен TabBarController c двумя NavigationController
            4. Изменены иконки у Tab Bar Item
            5. Созданы FeedViewController и ProfileViewController
            6. Добавлен PostViewController с кнопкой
            7. Создана структура Post для передачи заголовка
            8. Добавлен Bar Button Item для PostViewController
            9. Добавлен UIAlertController

---
+++ Navigation/Navigation/Profile/ProfileHeaderView.swift
@@ -0,0 +1,85 @@
+//
+//  ProfileHeaderView.swift
+//  Navigation
+//
+//  Created by Семён Пряничников on 30.03.2022.
+//
+
+import UIKit
+
+class ProfileHeaderView: UIView {
+    
+    lazy var fullNameLabel: UILabel = {
+        let fullNameLabel = UILabel()
+        fullNameLabel.text = "Batman"
+        fullNameLabel.textColor = .black
+        fullNameLabel.font = UIFont(name: "HelveticaNeue-Bold", size: 18.0)
+        fullNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        return fullNameLabel
+    }()
+    
+    lazy var statusLabel: UILabel = {
+        let statusLabel = UILabel()
+        statusLabel.text = "Waiting for something..."
+        statusLabel.textColor = .black
+        statusLabel.font = UIFont(name: "HelveticaNeue", size: 14.0)
+        statusLabel.translatesAutoresizingMaskIntoConstraints = false
+        return statusLabel
+    }()
+    
+    lazy var avatarImageView: UIImageView = {
+        let avatarImageView = UIImageView()
+        avatarImageView.translatesAutoresizingMaskIntoConstraints = false
+        avatarImageView.layer.borderWidth = 3
+        avatarImageView.layer.borderColor = UIColor.white.cgColor
+        avatarImageView.layer.cornerRadius = 50
+        avatarImageView.clipsToBounds = true
+        avatarImageView.image = UIImage(named: "BatmanPhoto")
+        return avatarImageView
+    }()
+    
+    lazy var setStatusButton: UIButton = {
+        let setStatusButton = UIButton()
+        setStatusButton.layer.cornerRadius = 4
+        setStatusButton.translatesAutoresizingMaskIntoConstraints = false
+        setStatusButton.setTitle("Set status", for: .normal)
+        setStatusButton.setTitleColor(.white, for: .normal)
+        setStatusButton.backgroundColor = .systemBlue
+        setStatusButton.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+        return setStatusButton
+    }()
+    
+    @objc func buttonPressed() {
+        statusLabel.text = statusText
+    }
+    
+    lazy var statusTextField: UITextField = {
+        let statusTextField = UITextField()
+        statusTextField.translatesAutoresizingMaskIntoConstraints = false
+        statusTextField.layer.cornerRadius = 12
+        statusTextField.backgroundColor = .white
+        statusTextField.font = UIFont(name: "HelveticaNeue", size: 15.0)
+        statusTextField.textColor = .black
+        statusTextField.layer.borderWidth = 1
+        statusTextField.layer.borderColor = UIColor.black.cgColor
+        statusTextField.addTarget(self, action: #selector(statusTextChanged), for: .editingChanged)
+        statusTextField.clipsToBounds = true
+        return statusTextField
+    }()
+    
+    private var statusText = ""
+    
+    @objc func statusTextChanged(_ textField: UITextField) {
+        if let text = statusTextField.text {
+            statusText = text
+        }
+    }
+    /*
+     // Only override draw() if you perform custom drawing.
+     // An empty implementation adversely affects performance during animation.
+     override func draw(_ rect: CGRect) {
+     // Drawing code
+     }
+     */
+    
+}
