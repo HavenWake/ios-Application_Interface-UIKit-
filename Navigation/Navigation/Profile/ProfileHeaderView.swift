a7b719e97abf764eb664d9b94208ded8417dbd30
Haven <havenwake@icloud.com>
2022-04-14 09:00:27 +0300

    code refactoring

--- Navigation/Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Navigation/Profile/ProfileHeaderView.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ProfileHeaderView: UIView {
-
+    
+    override init(frame: CGRect) {
+        super .init(frame: frame)
+        setupView()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     lazy var fullNameLabel: UILabel = {
         let fullNameLabel = UILabel()
         fullNameLabel.text = "Batman"
         fullNameLabel.textColor = .black
-        fullNameLabel.font = UIFont(name: "HelveticaNeue-Bold", size: 18.0)
+        fullNameLabel.font = UIFont.boldSystemFont(ofSize: 18)
         fullNameLabel.translatesAutoresizingMaskIntoConstraints = false
         return fullNameLabel
     }()
@@ -22,7 +32,7 @@ class ProfileHeaderView: UIView {
         let statusLabel = UILabel()
         statusLabel.text = "Waiting for something..."
         statusLabel.textColor = .black
-        statusLabel.font = UIFont(name: "HelveticaNeue", size: 14.0)
+        statusLabel.font = UIFont.systemFont(ofSize: 14)
         statusLabel.translatesAutoresizingMaskIntoConstraints = false
         return statusLabel
     }()
@@ -58,7 +68,7 @@ class ProfileHeaderView: UIView {
         statusTextField.translatesAutoresizingMaskIntoConstraints = false
         statusTextField.layer.cornerRadius = 12
         statusTextField.backgroundColor = .white
-        statusTextField.font = UIFont(name: "HelveticaNeue", size: 15.0)
+        statusTextField.font = UIFont.systemFont(ofSize: 15)
         statusTextField.textColor = .black
         statusTextField.layer.borderWidth = 1
         statusTextField.layer.borderColor = UIColor.black.cgColor
@@ -74,7 +84,7 @@ class ProfileHeaderView: UIView {
             statusText = text
         }
     }
-
+    
     lazy var newButton: UIButton = {
         let setStatusButton = UIButton()
         setStatusButton.layer.cornerRadius = 4
@@ -84,13 +94,50 @@ class ProfileHeaderView: UIView {
         setStatusButton.backgroundColor = .systemBlue
         return setStatusButton
     }()
-
-    /*
-     // Only override draw() if you perform custom drawing.
-     // An empty implementation adversely affects performance during animation.
-     override func draw(_ rect: CGRect) {
-     // Drawing code
-     }
-     */
     
+    func setupView() {
+        translatesAutoresizingMaskIntoConstraints = false
+        addSubview(setStatusButton)
+        addSubview(fullNameLabel)
+        addSubview(avatarImageView)
+        addSubview(statusLabel)
+        addSubview(statusTextField)
+        addSubview(newButton)
+        
+        //Настройка тени
+        layer.shadowOffset.width = 4
+        layer.shadowOffset.height = 4
+        layer.shadowRadius = 4
+        layer.shadowOpacity = 0.7
+        layer.shadowColor = UIColor.black.cgColor
+    }
+    func setupConstraints() {
+        //Для кнопки установки статуса
+        setStatusButton.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 46).isActive = true
+        setStatusButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16).isActive = true
+        setStatusButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16).isActive = true
+        setStatusButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        //Для лейбла с именем
+        fullNameLabel.topAnchor.constraint(equalTo:  topAnchor, constant: 27).isActive = true
+        fullNameLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16).isActive = true
+        fullNameLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 20).isActive = true
+        
+        //Для аватарки
+        avatarImageView.topAnchor.constraint(equalTo: topAnchor, constant: 16).isActive = true
+        avatarImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16).isActive = true
+        avatarImageView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        avatarImageView.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        
+        //Для лейбла со статусом
+        statusLabel.bottomAnchor.constraint(equalTo: statusTextField.topAnchor, constant: -16).isActive = true
+        statusLabel.trailingAnchor.constraint(equalTo: fullNameLabel.trailingAnchor).isActive = true
+        statusLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 20).isActive = true
+        
+        //Для текстфилда статуса
+        statusTextField.bottomAnchor.constraint(equalTo: setStatusButton.topAnchor, constant: -16).isActive = true
+        statusTextField.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16).isActive = true
+        statusTextField.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 20).isActive = true
+        statusTextField.heightAnchor.constraint(equalToConstant: 40).isActive = true
+    }
 }
--- Navigation/Navigation/Profile/ProfileViewController.swift
+++ Navigation/Navigation/Profile/ProfileViewController.swift
@@ -19,12 +19,11 @@ class ProfileViewController: UIViewController {
         self.topView.backgroundColor = .white
         self.view.addSubview(self.profileHeaderView)
         self.topView.frame = CGRect(x: 0, y: 0, width: self.view.bounds.width, height: 100)
-        profileHeaderViewSetup()
         setupConstraints()
     }
     
     override func viewWillLayoutSubviews() {
-
+        
     }
     
     private func setupConstraints() {
@@ -33,66 +32,10 @@ class ProfileViewController: UIViewController {
         self.profileHeaderView.rightAnchor.constraint(equalTo: self.view.rightAnchor).isActive = true
         self.profileHeaderView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor).isActive = true
         self.profileHeaderView.heightAnchor.constraint(equalToConstant: 220).isActive = true
-
-
-        //Для кнопки установки статуса
-        self.profileHeaderView.setStatusButton.topAnchor.constraint(equalTo: profileHeaderView.avatarImageView.bottomAnchor, constant: 46).isActive = true
-        self.profileHeaderView.setStatusButton.trailingAnchor.constraint(equalTo: profileHeaderView.trailingAnchor, constant: -16).isActive = true
-        self.profileHeaderView.setStatusButton.leadingAnchor.constraint(equalTo: profileHeaderView.leadingAnchor, constant: 16).isActive = true
-        self.profileHeaderView.setStatusButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
-
+        
         //Для newButton
         self.profileHeaderView.newButton.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor).isActive = true
         self.profileHeaderView.newButton.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
         self.profileHeaderView.newButton.rightAnchor.constraint(equalTo: self.view.rightAnchor).isActive = true
-        
-        //Для лейбла с именем
-        self.profileHeaderView.fullNameLabel.topAnchor.constraint(equalTo:  profileHeaderView.topAnchor, constant: 27).isActive = true
-        self.profileHeaderView.fullNameLabel.trailingAnchor.constraint(equalTo: profileHeaderView.trailingAnchor, constant: -16).isActive = true
-        self.profileHeaderView.fullNameLabel.leadingAnchor.constraint(equalTo: profileHeaderView.avatarImageView.trailingAnchor, constant: 20).isActive = true
-        
-        //Для аватарки
-        self.profileHeaderView.avatarImageView.topAnchor.constraint(equalTo: profileHeaderView.topAnchor, constant: 16).isActive = true
-        self.profileHeaderView.avatarImageView.leadingAnchor.constraint(equalTo: profileHeaderView.leadingAnchor, constant: 16).isActive = true
-        self.profileHeaderView.avatarImageView.heightAnchor.constraint(equalToConstant: 100).isActive = true
-        self.profileHeaderView.avatarImageView.widthAnchor.constraint(equalToConstant: 100).isActive = true
-        
-        //Для лейбла со статусом
-        self.profileHeaderView.statusLabel.bottomAnchor.constraint(equalTo: profileHeaderView.statusTextField.topAnchor, constant: -16).isActive = true
-        self.profileHeaderView.statusLabel.trailingAnchor.constraint(equalTo: profileHeaderView.fullNameLabel.trailingAnchor).isActive = true
-        self.profileHeaderView.statusLabel.leadingAnchor.constraint(equalTo: profileHeaderView.avatarImageView.trailingAnchor, constant: 20).isActive = true
-        
-        //Для текстфилда статуса
-        self.profileHeaderView.statusTextField.bottomAnchor.constraint(equalTo: profileHeaderView.setStatusButton.topAnchor, constant: -16).isActive = true
-        self.profileHeaderView.statusTextField.trailingAnchor.constraint(equalTo: profileHeaderView.trailingAnchor, constant: -16).isActive = true
-        self.profileHeaderView.statusTextField.leadingAnchor.constraint(equalTo: profileHeaderView.avatarImageView.trailingAnchor, constant: 20).isActive = true
-        self.profileHeaderView.statusTextField.heightAnchor.constraint(equalToConstant: 40).isActive = true
     }
-
-    func profileHeaderViewSetup() {
-        self.profileHeaderView.translatesAutoresizingMaskIntoConstraints = false
-        self.profileHeaderView.addSubview(self.profileHeaderView.setStatusButton)
-        self.profileHeaderView.addSubview(self.profileHeaderView.fullNameLabel)
-        self.profileHeaderView.addSubview(self.profileHeaderView.avatarImageView)
-        self.profileHeaderView.addSubview(self.profileHeaderView.statusLabel)
-        self.profileHeaderView.addSubview(self.profileHeaderView.statusTextField)
-        self.profileHeaderView.addSubview(self.profileHeaderView.newButton)
-
-        //Настройка тени
-        self.profileHeaderView.layer.shadowOffset.width = 4
-        self.profileHeaderView.layer.shadowOffset.height = 4
-        self.profileHeaderView.layer.shadowRadius = 4
-        self.profileHeaderView.layer.shadowOpacity = 0.7
-        self.profileHeaderView.layer.shadowColor = UIColor.black.cgColor
-    }
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
 }
